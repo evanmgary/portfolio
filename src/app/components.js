f945882eb4bc8d0b0fd561c5160bf1d8eda15bb4
Evan Gary <evanmgary@gmail.com>
2024-05-02 14:43:44 -0500

    add images, links

--- src/app/components.js
+++ src/app/components.js
@@ -1,26 +1,34 @@
 import Image from "next/image"
 import Link from "next/link"
-export function ProjectCardSm(){
+import "./styles.css"
+export function ProjectCardSm(props){
 
     return(
         <div className="project-card-sm">
             <Link href={props.link}>
-                <Image alt={""} src={props.location}/>
+                <div className="image-container">
+                    <Image className="sm-image" alt={""} src={props.location} fill={true}/>
+                </div>
                 <h3>{props.title}</h3>
             </Link>
+            <div className="card-desc-sm">{props.desc}</div>
         </div>
     )
 }
 
-export function ProjectCardLg(){
+export function ProjectCardLg(props){
 
     return(
         <div className="project-card-lg">
             <Link href={props.link} >
-                <Image alt={""} src={props.location}/>
-                <h3>{props.title}</h3>
+                <div className="image-container">
+                    <Image className="lg-image" alt={""} src={props.location} fill={true}/>
+                    <h3>{props.title}</h3>
+                </div>
             </Link>
+            <div className="card-desc-lg">{props.desc}</div>
         </div>
+        
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,42 @@
 import Image from "next/image";
+import Link from "next/link";
 import { ProjectCardLg, ProjectCardSm } from "./components";
+import "./styles.css"
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="portfolio-box-large">
+      <div className="name-info">
+        <h1 className="name">Evan Gary</h1>
+        <h3 className="tagline">Software Developer</h3>
+        <div className="links">
+            <div><Link className="git-link" href="http://github.com/evanmgary"><Image src="/github-mark.png" height="30" width="30" alt="Github logo"></Image>evanmgary</Link></div>
+            <div><Link className="lin-link" href="https://www.linkedin.com/in/evanmgary/"><Image src="/LinkedIn_icon.png" alt="Linkedin logo" height="30" width="30"></Image>evanmgary</Link></div>
+        </div>
         
+      </div>
+      <div className="portfolio-box-large">
+        <ProjectCardLg link="" location={"/bracket.PNG"} title="Bracket Maker" desc="Tool to create and randomize NCAA tournament brackets using various advanced stats" alt=""></ProjectCardLg>
+        <ProjectCardLg link="https://evanmgary.github.io/temperature-data-charter/" location={"/weather.PNG"} title="Weather App" desc="Easily view and chart weather data from a point and click map" alt=""></ProjectCardLg>      </div>
+
+      <div className="portfolio-box-large">
+        <ProjectCardLg link="" location={"/buoy.PNG"} title="Buoy Data Grapher" desc="Tool to obtain and chart NDBC buoy data" alt=""></ProjectCardLg>
+        <ProjectCardLg link="" location={"/pokemon.PNG"} title="Stat-Focused Pokedex" desc="Sortable Pokedex with competitive-focused statistics" alt=""></ProjectCardLg>
       </div>
       <div className="portfolio-box-large">
         
       </div>
-      <div className="portfolio-box-mini">
-        
+      <div className="portfolio-box-small">
+        <ProjectCardSm link="/quote" location={'/quote.PNG'} desc="Quote generator using an api" alt=""></ProjectCardSm>
+        <ProjectCardSm link="/markdown" location={'/markdown.PNG'} desc="Markdown using marked library" alt=""></ProjectCardSm>
+        <ProjectCardSm link="/drum" location={'/drum.PNG'} desc="Simple drum machine" alt=""></ProjectCardSm>
+      </div>
+      <div className="portfolio-box-small">
+        <ProjectCardSm link="/calculator" location={'/calculator.PNG'} desc="Javascript calculator" alt=""></ProjectCardSm>
+        <ProjectCardSm link="/clock" location={'/clock.PNG'} desc="Pomodoro timer" alt=""></ProjectCardSm>
+        <ProjectCardSm link="/gol" location={'/gol.PNG'} desc="Conway's game of life" alt=""></ProjectCardSm>
       </div>
     </main>
   );
